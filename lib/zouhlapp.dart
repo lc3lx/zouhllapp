3d7602499f586905b06de70d92f8ac9634cf805d
lc3lx <lc3lx@users.noreply.github.com>
2025-10-08 19:22:06 +0300

    env don

--- lib/zouhlapp.dart
+++ lib/zouhlapp.dart
@@ -7,6 +7,7 @@ class zouhlapp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
