6ade4f009b55fe5d5ce476d28cbfcc115589b8b3
lc3lx <lc3lx@users.noreply.github.com>
2025-10-08 13:23:46 +0300

    fist steup app

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:app/zouhlapp.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const zouhlapp());
+}
---
+++ lib/zouhlapp.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class zouhlapp extends StatelessWidget {
+  const zouhlapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Zouhlapp')),
+        body: Center(child: Text('Hello, World!')),
+      ),
+    );
+  }
+}
