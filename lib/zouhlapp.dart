45063cc131966853be1bfcfb8c38f55bd2edfe7a
lc3lx <lc3lx@users.noreply.github.com>
2025-10-09 03:21:09 +0300

    slash screen and screen utile done

--- lib/zouhlapp.dart
+++ lib/zouhlapp.dart
@@ -1,6 +1,7 @@
 import 'package:app/core/common/screens/no_network_screen.dart';
 import 'package:app/core/app/connectivity_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class zouhlapp extends StatelessWidget {
   const zouhlapp({super.key});
@@ -11,25 +12,28 @@ class zouhlapp extends StatelessWidget {
       valueListenable: ConnectivityController.instance.isConnected,
       builder: (_, value, __) {
         if (value) {
-          return MaterialApp(
-            title: 'Zouhlapp',
-            debugShowCheckedModeBanner: false,
-            theme: ThemeData(
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-            ),
-            builder: (context, widget) {
-              return Scaffold(
-                body: Builder(
-                  builder: (context) {
-                    ConnectivityController.instance.init();
-                    return widget!;
-                  },
-                ),
-              );
-            },
-            home: Scaffold(
-              appBar: AppBar(title: Text('Zouhlapp')),
-              body: Center(child: Text('Hello, World!')),
+          return ScreenUtilInit(
+            designSize: const Size(376, 812),
+            child: MaterialApp(
+              title: 'Zouhlapp',
+              debugShowCheckedModeBanner: false,
+              theme: ThemeData(
+                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              ),
+              builder: (context, widget) {
+                return Scaffold(
+                  body: Builder(
+                    builder: (context) {
+                      ConnectivityController.instance.init();
+                      return widget!;
+                    },
+                  ),
+                );
+              },
+              home: Scaffold(
+                appBar: AppBar(title: Text('Zouhlapp')),
+                body: Center(child: Text('Hello, World!')),
+              ),
             ),
           );
         } else {
